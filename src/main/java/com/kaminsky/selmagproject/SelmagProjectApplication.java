079776e2baf4dfd9671689f32c41024456271fe9
Philip Kaminsky <kaminsky8836@gmail.com>
2025-01-09 12:21:11 +0300

    initial commit

---
+++ src/main/java/com/kaminsky/selmagproject/SelmagProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kaminsky.selmagproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelmagProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelmagProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kaminsky/selmagproject/SelmagProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaminsky.selmagproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelmagProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
