e7c2734afdf0c8036f6385b15248ab0f6d6e4e65
Philip Kaminsky <kaminsky8836@gmail.com>
2025-03-21 15:56:04 +0300

    create customer app

--- customer-app/src/main/java/com/kaminsky/customerapp/CustomerAppApplication.java
+++ customer-app/src/main/java/com/kaminsky/customerapp/CustomerAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CustomerAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CustomerAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerAppApplication.class, args);
+    }
 
 }
